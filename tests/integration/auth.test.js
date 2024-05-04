786015a23a2962d5a307691b8b2140afdbe0d968
FabiansDock <fabian20122001@gmail.com>
2024-05-04 09:51:39 +0530

    Refactored

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -14,30 +14,28 @@ describe('auth middleware', () => {
         server.close();
     });
 
+    const exec = () => {
+        return request(server)
+                .post('/api/genres')
+                .set('x-auth-token', token)
+                .send({ name: 'genre1'});
+    };
+
     describe('POST /api/genres', () => {
         it('should return 401 if no token is provided', async () => {
             token = '';
-            const res = await request(server)
-                                .post('/api/genres')
-                                .set('x-auth-token', token)
-                                .send({});
+            const res = await exec();
             expect(res.status).toBe(401);
         });
         
         it('should return 400 if invalid token is provided', async () => {
             token = 'a';
-            const res = await request(server)
-                                .post('/api/genres')
-                                .set('x-auth-token', token)
-                                .send({});
+            const res = await exec();
             expect(res.status).toBe(400);
         });
         
         it('should return 200 if valid token is provided', async () => {
-            const res = await request(server)
-                                .post('/api/genres')
-                                .set('x-auth-token', token)
-                                .send({});
+            const res = await exec();
             expect(res.status).toBe(200);
         });
     });
